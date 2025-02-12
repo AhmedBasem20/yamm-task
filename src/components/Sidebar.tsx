438933cfc231cf3500f5272a644a47588cd95e69
AhmedBasem <demhamesab@gmail.com>
2025-02-13 00:29:18 +0200

    Sidebar improvements

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,57 +1,76 @@
 import { useState } from "react";
-import { Drawer, List, ListItem, ListItemText, IconButton, AppBar, Toolbar, Typography } from "@mui/material";
+import { Drawer, List, ListItem, ListItemText, IconButton, AppBar, Toolbar, Typography, ListItemButton, ListItemIcon, Divider } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
-
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import YammLogo from '../assets/yamm.svg'
 const Sidebar = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+    const [mobileOpen, setMobileOpen] = useState(false);
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
-  const toggleDrawer = () => {
-    setMobileOpen(!mobileOpen);
-  };
+    const toggleDrawer = () => {
+        setMobileOpen(!mobileOpen);
+    };
 
-  const drawerContent = (
-    <List>
-      <ListItem component={Link} to="/" onClick={toggleDrawer}>
-        <ListItemText primary="Refund Orders" />
-      </ListItem>
-    </List>
-  );
+    const drawerContent = (
+        <>
+            {isMobile && <Toolbar />}
 
-  return (
-    <>
-      {isMobile && (
-        <AppBar position="fixed" sx={{ zIndex: theme.zIndex.drawer + 1 }}>
-          <Toolbar>
-            <IconButton edge="start" color="inherit" onClick={toggleDrawer} sx={{ marginRight: 2 }}>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6">Dashboard</Typography>
-          </Toolbar>
-        </AppBar>
-      )}
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: '16px'
+            }}>
+                <img src={YammLogo} />
+            </div>
+            <Divider />
+            <List>
+                <ListItem component={Link} to="/" onClick={toggleDrawer}>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <ReceiptLongIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Refund Orders" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </>
+    );
 
-      <Drawer
-        variant={isMobile ? "temporary" : "permanent"}
-        open={isMobile ? mobileOpen : true}
-        onClose={toggleDrawer}
-        sx={{
-          width: "250px",
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: "250px",
-            boxSizing: "border-box",
-          },
-        }}
-      >
-        {drawerContent}
-      </Drawer>
-    </>
-  );
+    return (
+        <>
+            {isMobile && (
+                <AppBar position="fixed" sx={{ zIndex: theme.zIndex.drawer + 1 }}>
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" onClick={toggleDrawer} sx={{ marginRight: 2 }}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6">Dashboard</Typography>
+                    </Toolbar>
+                </AppBar>
+            )}
+
+            <Drawer
+                variant={isMobile ? "temporary" : "permanent"}
+                open={isMobile ? mobileOpen : true}
+                onClose={toggleDrawer}
+                sx={{
+                    width: "250px",
+                    flexShrink: 0,
+                    "& .MuiDrawer-paper": {
+                        width: "250px",
+                        boxSizing: "border-box",
+                    },
+                }}
+            >
+                {drawerContent}
+            </Drawer>
+        </>
+    );
 };
 
 export default Sidebar;
