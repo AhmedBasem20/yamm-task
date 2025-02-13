b46c09de9e62e64c3e4ce956f08d6c50cc3ce7c1
AhmedBasem <demhamesab@gmail.com>
2025-02-13 16:49:39 +0200

    Add table pagination. Added pagination optional parameter to the DataTable component, if set to false, the pagination won't display

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,50 +1,83 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow } from "@mui/material";
 import { TableHeader } from "../types";
+import { useState } from "react";
 
 interface TableProps<T> {
     headers: TableHeader<T>[];
     data: T[];
+    pagination?: boolean
 }
 
 const DataTable = <T extends object>({
     headers,
-    data
+    data,
+    pagination = true
 }: TableProps<T>) => {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+        console.log(event)
+    };
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
 
     return (
-        <TableContainer
-            component={Paper}
-            sx={{
-                overflowX: "auto",
-                maxWidth: "100%",
-                borderRadius: "10px",
-                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-            }}
-        >
-            <Table>
-                <TableHead sx={{ backgroundColor: "#EBD9FF" }}>
-                    <TableRow >
-                        {headers.map((header: TableHeader<T>) => (
-                            <TableCell sx={{
-                                fontWeight: "bold",
-                                color: "#6C27FF",
-                                backgroundColor: "#EBD9FF",
-                            }} key={header.id}>{header.label}</TableCell>
-                        ))}
-
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.map((row) => (
-                        <TableRow>
+        <>
+            <TableContainer
+                component={Paper}
+                sx={{
+                    overflowX: "auto",
+                    maxWidth: "100%",
+                    borderRadius: "10px",
+                    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+                }}
+            >
+                <Table>
+                    <TableHead sx={{ backgroundColor: "#EBD9FF" }}>
+                        <TableRow >
                             {headers.map((header: TableHeader<T>) => (
-                                <TableCell key={header.id}>{header.render ? header.render(row) : row[header.id]}</TableCell>
+                                <TableCell sx={{
+                                    fontWeight: "bold",
+                                    color: "#6C27FF",
+                                    backgroundColor: "#EBD9FF",
+                                }} key={header.id}>{header.label}</TableCell>
                             ))}
+
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {data
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row) => (
+                                <TableRow>
+                                    {headers.map((header: TableHeader<T>) => (
+                                        <TableCell key={header.id}>{header.render ? header.render(row) : row[header.id]}</TableCell>
+                                    ))}
+                                </TableRow>
+                            ))}
+                    </TableBody>
+                </Table>
+                {pagination &&
+                <TableFooter>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 15]}
+                        component="div"
+                        count={data.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+
+                    />
+
+                </TableFooter>
+                }
+            </TableContainer>
+        </>
     );
 };
 
